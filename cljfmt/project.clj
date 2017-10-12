d650d05c5862e86de55dff6f4ad416ca686ef3e6
Greg Look <greg@greg-look.net>
2017-10-12 12:01:59 -0700

    Namespace project coordiantes.

--- cljfmt/project.clj
+++ cljfmt/project.clj
@@ -1,15 +1,21 @@
-(defproject cljfmt "0.5.6"
+(defproject mvxcvi/cljfmt "0.5.6-SNAPSHOT"
   :description "A library for formatting Clojure code"
-  :url "https://github.com/weavejester/cljfmt"
+  :url "https://github.com/greglook/cljfmt"
   :scm {:dir ".."}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.reader "1.0.0-alpha4"]
-                 [rewrite-clj "0.5.2"]
-                 [rewrite-cljs "0.4.3"]]
-  :plugins [[lein-cljsbuild "1.1.2"]]
+
+  :plugins
+  [[lein-cljsbuild "1.1.2"]]
+
+  :dependencies
+  [[org.clojure/clojure "1.8.0"]
+   [org.clojure/tools.reader "1.0.0-alpha4"]
+   [rewrite-clj "0.5.2"]
+   [rewrite-cljs "0.4.3"]]
+
   :hooks [leiningen.cljsbuild]
+
   :cljsbuild {:builds
               {"dev" {:source-paths ["src" "test"]
                       :compiler {:main cljfmt.test-runner
@@ -19,5 +25,7 @@
                                  :optimizations :none}}}
               :test-commands
               {"dev" ["node" "target/out/tests.js"]}}
+
   :profiles
-  {:provided {:dependencies [[org.clojure/clojurescript "1.7.228"]]}})
+  {:provided
+   {:dependencies [[org.clojure/clojurescript "1.7.228"]]}})
--- lein-cljfmt/project.clj
+++ lein-cljfmt/project.clj
@@ -1,10 +1,13 @@
-(defproject lein-cljfmt "0.5.6"
+(defproject mvxcvi/lein-cljfmt "0.5.6-SNAPSHOT"
   :description "A library for formatting Clojure code"
-  :url "https://github.com/weavejester/cljfmt"
+  :url "https://github.com/greglook/cljfmt"
   :scm {:dir ".."}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :eval-in-leiningen true
-  :dependencies [[cljfmt "0.5.6"]
-                 [meta-merge "0.1.1"]
-                 [com.googlecode.java-diff-utils/diffutils "1.2.1"]])
+
+  :dependencies
+  [[mvxcvi/cljfmt "0.5.6"]
+   [meta-merge "0.1.1"]
+   [com.googlecode.java-diff-utils/diffutils "1.2.1"]])
