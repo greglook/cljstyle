c8e6a58d15acad7710e63bb065fe1bfc8126b8eb
Greg Look <greg@greg-look.net>
2017-11-28 00:47:19 -0800

    Bump version to 0.7.0-SNAPSHOT.

--- cljfmt/project.clj
+++ cljfmt/project.clj
@@ -1,4 +1,4 @@
-(defproject mvxcvi/cljfmt "0.5.6-SNAPSHOT"
+(defproject mvxcvi/cljfmt "0.7.0-SNAPSHOT"
   :description "A library for formatting Clojure code"
   :url "https://github.com/greglook/cljfmt"
   :scm {:dir ".."}
--- lein-cljfmt/project.clj
+++ lein-cljfmt/project.clj
@@ -1,4 +1,4 @@
-(defproject mvxcvi/lein-cljfmt "0.5.6-SNAPSHOT"
+(defproject mvxcvi/lein-cljfmt "0.7.0-SNAPSHOT"
   :description "A library for formatting Clojure code"
   :url "https://github.com/greglook/cljfmt"
   :scm {:dir ".."}
@@ -8,6 +8,6 @@
   :eval-in-leiningen true
 
   :dependencies
-  [[mvxcvi/cljfmt "0.5.6-SNAPSHOT"]
+  [[mvxcvi/cljfmt "0.7.0-SNAPSHOT"]
    [meta-merge "0.1.1"]
    [com.googlecode.java-diff-utils/diffutils "1.2.1"]])
