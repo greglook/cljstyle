808cef05b62debf13be1272e07b6699378ed9fe7
Greg Look <greg@greg-look.net>
2017-11-27 20:36:44 -0800

    Add some repl helpers.

--- cljfmt/dev/user.clj
+++ cljfmt/dev/user.clj
@@ -1,8 +1,33 @@
 (ns user
   (:require
+    [cljfmt.core :as core]
+    [cljfmt.zloc :as zl]
+    [clojure.java.io :as io]
     [clojure.repl :refer :all]
     [clojure.string :as str]
-    [clojure.tools.namespace.repl :refer [refresh]]))
+    [clojure.tools.namespace.repl :refer [refresh]]
+    [clojure.zip :as zip]
+    [rewrite-clj.node :as n]
+    [rewrite-clj.parser :as p]
+    [rewrite-clj.zip :as z]))
 
 
-; TODO: utility to show forms matching predicates in a string
+(defn zfind
+  [form-string p?]
+  (->> (p/parse-string-all form-string)
+       (z/edn)
+       (iterate #(z/find-next % zip/next p?))
+       ;(iterate #(z/find-next-depth-first % p?))
+       (next)
+       (take-while some?)
+       (map z/string)))
+
+
+(defn try-edit
+  [form-string edit-fn & args]
+  (println "Initial:\n" form-string)
+  (println "Formatted:")
+  (-> (p/parse-string-all form-string)
+      (as-> x (apply edit-fn x args))
+      (n/string)
+      (println)))
