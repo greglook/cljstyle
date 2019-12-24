f4a4cfaf44407b4cb416445c8edba3e8ba758c0c
Ace Levenberg <ace@amperity.com>
2019-12-23 16:19:50 -0800

    cljstyle...

--- core/src/cljstyle/task/core.clj
+++ core/src/cljstyle/task/core.clj
@@ -16,162 +16,101 @@
 ;; ## Utilities
 
 (defn- search-roots
-  "Convert the list of paths into a collection of search
-                    roots. If the path list is empty, uses the local directory
-                    as a single root." [paths]
-  (mapv io/file (or (seq paths)
-                    ["."])))
+  "Convert the list of paths into a collection of search roots. If the path
+  list is empty, uses the local directory as a single root."
+  [paths]
+  (mapv io/file (or (seq paths) ["."])))
 
 
 (defn- load-configs
-  "Load parent configuration files. Returns a merged
-                    configuration map." [label ^File file]
-  (let [configs
-        (config/find-parents file 25)]
-    (if (seq
-          configs) (p/logf "Using cljstyle
-                    configuration from %d sources for %s:\n%s" (count configs)
-                           label (str/join
-                                   "\n" (mapcat
-                                          config/source-paths
-                                          configs)))
-        (p/logf "Using default cljstyle configuration for %s" label)) (apply
-                                                                        config/merge-settings
-                                                                        config/default-config
-                                                                        configs)))
+  "Load parent configuration files. Returns a merged configuration map."
+  [label ^File file]
+  (let [configs (config/find-parents file 25)]
+    (if (seq configs)
+      (p/logf "Using cljstyle configuration from %d sources for %s:\n%s"
+              (count configs)
+              label
+              (str/join "\n" (mapcat config/source-paths configs)))
+      (p/logf "Using default cljstyle configuration for %s"
+              label))
+    (apply config/merge-settings config/default-config configs)))
 
 
 (defn- walk-files!
-  "Walk source files and apply the processing function to
-                    each." [f paths]
-  (->> (search-roots paths) (pmap (fn prep-root
-                                    [^File root]
-                                    (let [canonical
-                                          (.getCanonicalFile root)]
-                                      [(load-configs (.getPath
-                                                       root) canonical) root canonical])))
-       (process/walk-files! f)))
+  "Walk source files and apply the processing function to each."
+  [f paths]
+  (->>
+    (search-roots paths)
+    (pmap (fn prep-root
+            [^File root]
+            (let [canonical (.getCanonicalFile root)]
+              [(load-configs (.getPath root) canonical) root canonical])))
+    (process/walk-files! f)))
 
 
 (defn- write-stats!
   "Write stats output to the named file."
   [file-name stats]
   (let [ext (last (str/split file-name #"\."))]
-    (case ext "edn" (spit file-name
-                          (prn-str
-                            stats))
+    (case ext
+      "edn"
+      (spit file-name (prn-str stats))
 
-          "tsv" (->> (:files stats) (into (dissoc stats :files) (map (fn [[k v]]
-                                                                       [(keyword
-                                                                          "files"
-                                                                          (name k))
-                                                                        v]))) (map
-                                                                                (fn [[k
-                                                                                      v]]
-                                                                                  (str
-                                                                                    (subs
-                                                                                      (str
-                                                                                        k)
-                                                                                      1)
-                                                                                    \tab
-                                                                                    v
-                                                                                    \newline)))
-                     (str/join) (spit file-name))
+      "tsv"
+      (->> (:files stats)
+           (into (dissoc stats :files)
+                 (map (fn [[k v]]
+                        [(keyword "files" (name k)) v])))
+           (map (fn [[k v]] (str (subs (str k) 1) \tab v \newline)))
+           (str/join)
+           (spit file-name))
 
       ;; else
-          (p/printerrf "Unknown stats file extension '%s' - ignoring!" ext))))
+      (p/printerrf "Unknown stats file extension '%s' - ignoring!" ext))))
 
 
 (defn- report-stats
   "General result reporting logic."
   [results]
-  (let [counts
-        (:counts
-          results)
-        total-files
-        (apply +
-               (vals
-                 counts))
-        diff-lines
-        (apply +
-               (keep
-                 :diff-lines
-                 (vals
-                   (:results
-                     results))))
-        stats
-        (cond->
-          {:files
-           counts
-           :total
-           total-files
-           :elapsed
-           (:elapsed
-             results)}
-          (pos?
-            diff-lines)
-          (assoc
-            :diff-lines
-            diff-lines))]
-    (p/logf
-      "Checked %d
-                                                                    files in
-                                                                    %.2f ms"
-      total-files
-      (:elapsed
-        results
-        -1.0))
-    (p/log
-      (pr-str
-        stats))
-    (when-let
-      [stats-file
-       (p/option
-         :stats)]
-      (write-stats!
-        stats-file
-        stats))))
+  (let [counts (:counts results)
+        total-files (apply + (vals counts))
+        diff-lines (apply + (keep :diff-lines (vals (:results results))))
+        stats (cond-> {:files counts
+                       :total total-files
+                       :elapsed (:elapsed results)}
+                (pos? diff-lines)
+                (assoc :diff-lines diff-lines))]
+    (p/logf "Checked %d files in %.2f ms"
+            total-files
+            (:elapsed results -1.0))
+    (p/log (pr-str stats))
+    (when-let [stats-file (p/option :stats)]
+      (write-stats! stats-file stats))))
 
 
 ;; ## Version Command
 
-(def version "Project version string." (if-let [props-file (io/resource
-                                                             "META-INF/maven/mvxcvi/cljstyle/pom.properties")]
-                                         (with-open [props-reader (io/reader
-                                                                    props-file)]
-                                           (let [props (doto
-                                                         (java.util.Properties.)
-                                                         (.load props-reader))
-                                                 {:strs [groupId artifactId
-                                                         version revision]}
-                                                 props] (format "%s/%s %s (%s)"
-                                                                groupId
-                                                                artifactId
-                                                                version
-                                                                (str/trim-newline
-                                                                  revision))))
-                                         "HEAD"))
+(def version
+  "Project version string."
+  (if-let [props-file (io/resource "META-INF/maven/mvxcvi/cljstyle/pom.properties")]
+    (with-open [props-reader (io/reader props-file)]
+      (let [props (doto (java.util.Properties.)
+                    (.load props-reader))
+            {:strs [groupId artifactId version revision]} props]
+        (format "%s/%s %s (%s)"
+                groupId artifactId version
+                (str/trim-newline revision))))
+    "HEAD"))
 
 
 (defn print-version
   "Implementation of the `version` command."
   [args]
-  (when
-    (seq
-      args)
-    (binding
-      [*out*
-       *err*]
-      (println
-        "cljstyle
-                                                                            version
-                                                                            command
-                                                                            takes
-                                                                            no
-                                                                            arguments")
+  (when (seq args)
+    (binding [*out* *err*]
+      (println "cljstyle version command takes no arguments")
       (flush)
-      (System/exit
-        1)))
+      (System/exit 1)))
   (println version)
   (flush))
 
