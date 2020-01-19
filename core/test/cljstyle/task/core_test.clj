b7d34d34fba2e07ca2ad5af5a2e825ef63f9ca57
Greg Look <greg@amperity.com>
2020-01-19 12:03:02 -0800

    Add tests for check task.

--- core/test/cljstyle/task/core_test.clj
+++ core/test/cljstyle/task/core_test.clj
@@ -1,6 +1,7 @@
 (ns cljstyle.task.core-test
   (:require
     [cljstyle.task.core :as task]
+    [cljstyle.task.print :as p]
     [cljstyle.test-util :refer [with-files]]
     [clojure.java.io :as io]
     [clojure.string :as str]
@@ -73,8 +74,10 @@
       (capture-io
         (is (= {:unrelated 1, :found 2}
                (:counts (task/find-sources [(.getPath test-dir)]))))
-        (is (= "target/test-config/find/a/x/bar.clj\ntarget/test-config/find/a/b/foo.clj\n"
-               stdout))
+        (let [lines (set (str/split stdout #"\n"))]
+          (is (= 2 (count lines)))
+          (is (contains? lines "target/test-config/find/a/b/foo.clj"))
+          (is (contains? lines "target/test-config/find/a/x/bar.clj")))
         (is (str/blank? stderr))))))
 
 
@@ -85,7 +88,61 @@
       (is (str/starts-with? stdout "Usage: cljstyle [options] check "))
       (is (str/blank? stderr))))
   (testing "output"
-    ,,,))
+    (with-files [test-dir "target/test-config/check"
+                 a-config ["a/.cljstyle" (prn-str {:rewrite-namespaces? true})]
+                 foo-clj ["a/b/foo.clj" ";; foo\n"]
+                 bar-clj ["a/x/bar.clj" "(ns a.x.bar\n  (:require\n    [clojure.string :as str]))\n"]]
+      (testing "when correct"
+        (capture-io
+          (is (map? (task/check-sources [(str test-dir)])))
+          (is (str/blank? stdout))
+          (is (str/blank? stderr))))
+      (testing "when incorrect"
+        (spit foo-clj "(def abc \"a doc\" 123)")
+        (capture-io
+          (is (thrown-with-data? {:code 2}
+                (task/check-sources [(str test-dir)])))
+          (is (not (str/blank? stdout)))
+          (is (= "1 files formatted incorrectly\n" stderr))))
+      (testing "when error"
+        (spit foo-clj "(def abc ...")
+        (capture-io
+          (is (thrown-with-data? {:code 3}
+                (task/check-sources [(str test-dir)])))
+          (is (str/blank? stdout))
+          (is (str/starts-with? stderr "Error while processing file target/test-config/check/a/b/foo.clj\njava.lang.Exception: Unexpected EOF"))
+          (is (str/ends-with? stderr "Failed to process 1 files\n")))
+        (testing "verbose"
+          (capture-io
+            (p/with-options {:verbose true}
+              (is (thrown-with-data? {:code 3}
+                    (task/check-sources [(str test-dir)]))))
+            (is (str/blank? stdout))
+            (is (str/includes? stderr "Error while processing file target/test-config/check/a/b/foo.clj"))
+            (is (str/ends-with? stderr "Failed to process 1 files\n")))))
+      (testing "ignored file"
+        (spit a-config (prn-str {:file-ignore #{"foo.clj"}}))
+        (capture-io
+          (is (map? (task/check-sources [(str test-dir)])))
+          (is (str/blank? stdout))
+          (is (str/blank? stderr))))
+      (testing "stats output"
+        (testing "tsv"
+          (let [stats-file (io/file test-dir "stats.tsv")]
+            (capture-io
+              (p/with-options {:stats (str stats-file)}
+                (is (map? (task/check-sources [(str test-dir)]))))
+              (is (str/blank? stdout))
+              (is (str/blank? stderr))
+              (is (.exists stats-file)))))
+        (testing "edn"
+          (let [stats-file (io/file test-dir "stats.edn")]
+            (capture-io
+              (p/with-options {:stats (str stats-file)}
+                (is (map? (task/check-sources [(str test-dir)]))))
+              (is (str/blank? stdout))
+              (is (str/blank? stderr))
+              (is (.exists stats-file)))))))))
 
 
 (deftest fix-command
