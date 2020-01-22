1ae9b44384d24cfff94ce3233f029d059adf248f
Greg Look <greg@mvxcvi.com>
2020-01-21 22:33:14 -0800

    Bump SNAPSHOT version.

--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-(defproject mvxcvi/cljstyle "0.11.0"
+(defproject mvxcvi/cljstyle "0.11.1-SNAPSHOT"
   :description "A library for formatting Clojure code"
   :url "https://github.com/greglook/cljstyle"
   :scm {:dir ".."}
