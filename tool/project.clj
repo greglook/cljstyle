895ef01efef3b745bcd2019eb8b6cafe37f44cb6
Greg Look <greg@mvxcvi.com>
2020-01-22 18:02:36 -0800

    Bump SNAPSHOT version.

--- tool/project.clj
+++ tool/project.clj
@@ -1,4 +1,4 @@
-(defproject mvxcvi/cljstyle-tool "0.11.1"
+(defproject mvxcvi/cljstyle-tool "0.11.2-SNAPSHOT"
   :description "An executable tool for running cljstyle."
   :url "https://github.com/greglook/cljstyle"
   :scm {:dir ".."}
@@ -8,7 +8,7 @@
   :dependencies
   [[org.clojure/clojure "1.9.0"]
    [org.clojure/tools.cli "0.4.2"]
-   [mvxcvi/cljstyle "0.11.1"]]
+   [mvxcvi/cljstyle "0.11.2-SNAPSHOT"]]
 
   :main cljstyle.tool.main
 
