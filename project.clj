22ef8515ba9557bc22dfc31151d11018218d8eb3
Greg Look <greg@mvxcvi.com>
2020-06-04 22:58:09 -0700

    Upgrade clojure and lib versions.

--- project.clj
+++ project.clj
@@ -8,8 +8,8 @@
   [[lein-cloverage "1.1.0"]]
 
   :dependencies
-  [[org.clojure/clojure "1.9.0"]
-   [org.clojure/tools.cli "0.4.2"]
+  [[org.clojure/clojure "1.10.2-alpha1"]
+   [org.clojure/tools.cli "1.0.194"]
    [org.clojure/tools.reader "1.3.2"]
    [com.googlecode.java-diff-utils/diffutils "1.3.0"]
    [rewrite-clj "0.6.1"]]
@@ -30,8 +30,8 @@
     :jvm-opts ["-XX:+UnlockDiagnosticVMOptions"
                "-XX:+DebugNonSafepoints"]
     :dependencies
-    [[com.clojure-goes-fast/clj-async-profiler "0.4.0"]
-     [org.clojure/tools.namespace "0.3.1"]]}
+    [[com.clojure-goes-fast/clj-async-profiler "0.4.1"]
+     [org.clojure/tools.namespace "1.0.0"]]}
 
    :uberjar
    {:target-path "target/uberjar"
