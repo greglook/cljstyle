1234c4b9e306eb96f8908720180be5deb1582af0
Greg Look <greg@mvxcvi.com>
2020-10-26 16:52:47 -0700

    Update whitespace tests.

--- test/cljstyle/format/whitespace_test.clj
+++ test/cljstyle/format/whitespace_test.clj
@@ -1,119 +1,108 @@
 (ns cljstyle.format.whitespace-test
   (:require
-    [cljstyle.format.core-test :refer [with-test-config is-reformatted]]
+    [cljstyle.format.whitespace :as ws]
+    [cljstyle.test-util]
     [clojure.test :refer [deftest testing is]]))
 
 
 (deftest surrounding-whitespace
   (testing "surrounding spaces"
-    (is-reformatted
-      "( foo bar )"
-      "(foo bar)")
-    (is-reformatted
-      "[ 1 2 3 ]"
-      "[1 2 3]")
-    (is-reformatted
-      "{  :x 1, :y 2 }"
-      "{:x 1, :y 2}"))
+    (is (reformatted?
+          ws/remove-surrounding {}
+          "( foo bar )"
+          "(foo bar)"))
+    (is (reformatted?
+          ws/remove-surrounding {}
+          "[ 1 2 3 ]"
+          "[1 2 3]"))
+    (is (reformatted?
+          ws/remove-surrounding {}
+          "{  :x 1, :y 2 }"
+          "{:x 1, :y 2}")))
   (testing "surrounding newlines"
-    (is-reformatted
-      "(\n  foo\n)"
-      "(foo)")
-    (is-reformatted
-      "(  \nfoo\n)"
-      "(foo)")
-    (is-reformatted
-      "(foo  \n)"
-      "(foo)")
-    (is-reformatted
-      "(foo\n  )"
-      "(foo)")
-    (is-reformatted
-      "[\n1 2 3\n]"
-      "[1 2 3]")
-    (is-reformatted
-      "{\n:foo \"bar\"\n}"
-      "{:foo \"bar\"}")
-    (is-reformatted
-      "( let [x 3\ny 4]\n(+ (* x x\n)(* y y)\n))"
-      "(let [x 3\n      y 4]\n  (+ (* x x) (* y y)))")))
+    (is (reformatted?
+          ws/remove-surrounding {}
+          "(\n  foo\n)"
+          "(foo)"))
+    (is (reformatted?
+          ws/remove-surrounding {}
+          "(  \nfoo\n)"
+          "(foo)"))
+    (is (reformatted?
+          ws/remove-surrounding {}
+          "(foo  \n)"
+          "(foo)"))
+    (is (reformatted?
+          ws/remove-surrounding {}
+          "(foo\n  )"
+          "(foo)"))
+    (is (reformatted?
+          ws/remove-surrounding {}
+          "[\n1 2 3\n]"
+          "[1 2 3]"))
+    (is (reformatted?
+          ws/remove-surrounding {}
+          "{\n:foo \"bar\"\n}"
+          "{:foo \"bar\"}"))
+    (is (reformatted?
+          ws/remove-surrounding {}
+          "( let [x 3\ny 4]\n(+ (* x x\n)(* y y)\n))"
+          "(let [x 3\ny 4]\n(+ (* x x)(* y y)))"))))
 
 
 (deftest missing-whitespace
   (testing "collections"
-    (is-reformatted
-      "(foo(bar baz)qux)"
-      "(foo (bar baz) qux)")
-    (is-reformatted
-      "(foo)bar(baz)"
-      "(foo) bar (baz)")
-    (is-reformatted
-      "(foo[bar]#{baz}{quz bang})"
-      "(foo [bar] #{baz} {quz bang})"))
+    (is (reformatted?
+          ws/insert-missing {}
+          "(foo(bar baz)qux)"
+          "(foo (bar baz) qux)"))
+    (is (reformatted?
+          ws/insert-missing {}
+          "(foo)bar(baz)"
+          "(foo) bar (baz)"))
+    (is (reformatted?
+          ws/insert-missing {}
+          "(foo[bar]#{baz}{quz bang})"
+          "(foo [bar] #{baz} {quz bang})")))
   (testing "reader conditionals"
-    (is-reformatted
-      "#?(:cljs(bar 1) :clj(foo 2))"
-      "#?(:cljs (bar 1) :clj (foo 2))")
-    (is-reformatted
-      "#?@(:cljs[foo bar] :clj[baz quux])"
-      "#?@(:cljs [foo bar] :clj [baz quux])")))
+    (is (reformatted?
+          ws/insert-missing {}
+          "#?(:cljs(bar 1) :clj(foo 2))"
+          "#?(:cljs (bar 1) :clj (foo 2))"))
+    (is (reformatted?
+          ws/insert-missing {}
+          "#?@(:cljs[foo bar] :clj[baz quux])"
+          "#?@(:cljs [foo bar] :clj [baz quux])"))))
 
 
 (deftest trailing-whitespace
-  (testing "trailing-whitespace"
-    (is-reformatted
-      "(foo bar) "
-      "(foo bar)")
-    (is-reformatted
-      "(foo bar)\n"
-      "(foo bar)\n")
-    (is-reformatted
-      "(foo bar) \n "
-      "(foo bar)\n")
-    (is-reformatted
-      "(foo bar) \n(foo baz)"
-      "(foo bar)\n(foo baz)")
-    (is-reformatted
-      "(foo bar)\t\n(foo baz)"
-      "(foo bar)\n(foo baz)"))
-  (testing "preserve surrounding whitespace"
-    (with-test-config {:rules {:whitespace {:remove-surrounding? false}}}
-      (is-reformatted
+  (testing "with trailing whitespace"
+    (is (reformatted?
+          ws/remove-trailing {}
+          "(foo bar) "
+          "(foo bar)"))
+    (is (reformatted?
+          ws/remove-trailing {}
+          "(foo bar)\n"
+          "(foo bar)\n"))
+    (is (reformatted?
+          ws/remove-trailing {}
+          "(foo bar) \n "
+          "(foo bar)\n"))
+    (is (reformatted?
+          ws/remove-trailing {}
+          "(foo bar) \n(foo baz)"
+          "(foo bar)\n(foo baz)"))
+    (is (reformatted?
+          ws/remove-trailing {}
+          "(foo bar)\t\n(foo baz)"
+          "(foo bar)\n(foo baz)")))
+  (testing "with surrounding whitespace"
+    (is (reformatted?
+        ws/remove-trailing {}
         "( foo bar ) \n"
-        "( foo bar )\n")
-      (is-reformatted
+        "( foo bar )\n"))
+    (is (reformatted?
+        ws/remove-trailing {}
         "( foo bar )   \n( foo baz )\n"
         "( foo bar )\n( foo baz )\n"))))
-
-
-(deftest comma-placeholders
-  (testing "general usage"
-    (is-reformatted ",,," ",,,"))
-  (testing "collection tails"
-    (is-reformatted
-      "(def thing
-  [:one  ; a comment
-   :two  ; another comment
-   ,,,])"
-      "(def thing
-  [:one  ; a comment
-   :two  ; another comment
-   ,,,])")
-    (is-reformatted
-      "(def thing
-  #{:one 1  ; a comment
-    :two 2  ; another comment
-    ,,,})"
-      "(def thing
-  #{:one 1  ; a comment
-    :two 2  ; another comment
-    ,,,})")
-    (is-reformatted
-      "(def thing
-  {:one 1  ; a comment
-   :two 2  ; another comment
-   ,,,})"
-      "(def thing
-  {:one 1  ; a comment
-   :two 2  ; another comment
-   ,,,})")))
